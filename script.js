2dad1f2d0d18a973b7e735e14e5221f143d6f948
mohasarreb <mohasarreb2000@gmail.com>
2026-02-14 04:10:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+"use strict";
+
+const tontonGifs = [
+  "https://media.tenor.com/TUVAE2M_wz4AAAAi/chubby-tonton.gif",
+  "https://media.tenor.com/pZk_U5JVWzUAAAAi/tonton-friends-tonton.gif",
+  "https://media.tenor.com/Jkha__Yjf0oAAAAi/sad-depression.gif",
+  "https://media.tenor.com/U0OPHZokzkUAAAAi/what-seriously.gif",
+  "https://media.tenor.com/WKXMmSk3JJsAAAAi/chubby-tonton.gif",
+  "https://media.tenor.com/ZHWV13jliTAAAAAi/chubby-tonton.gif",
+];
+
+const title = document.querySelector(".title");
+const btnContainer = document.querySelector(".buttons");
+const yesBtn = document.querySelector(".btn-yes");
+const noBtn = document.querySelector(".btn-no");
+const img = document.querySelector(".img");
+
+const MAX_IMAGES = 5;
+let play = true;
+let noCount = 0;
+let noButtonSize = 1;
+let yesButtonSize = 1;
+
+yesBtn.addEventListener("click", () => {
+  title.innerHTML = "ليس علي أن أحبك في يومٍ محدد فقد أحببتك حبًا خالدًا أبديًا أزليًا وسرمديًاوإني بك هائمًا معجبًا مغرمًا متيمًا مجنونًا ومع غيرك صائمًا ثم إني سأحبك في كل أيام السنة وليس يومًا محددًا فقط فأنتي تستحقين أن يحبك أحدهم فوق العمر عمر وفوق الأيام أيام الى اميرتي و بنوتي و دلوعتي الصغيرة و جميلة قلبي I LOVE YOU SOOOOOOO MUCH 💗💗💗💗💗💗💗💗";
+  btnContainer.classList.add("hidden");
+  changeImage("yes");
+});
+
+noBtn.addEventListener("click", () => {
+  if (play) {
+    noCount++;
+    const imageIndex = Math.min(noCount, MAX_IMAGES);
+    changeImage(imageIndex);
+    resizeYesButton();
+    shrinkNoButton();
+    updateNoButtonText();
+    if (noCount === MAX_IMAGES) play = false;
+  }
+});
+
+function resizeYesButton() {
+  yesButtonSize *= 1.2;
+  yesBtn.style.transform = `scale(${yesButtonSize})`;
+}
+
+function shrinkNoButton() {
+  noButtonSize *= 0.90;
+  noBtn.style.transform = `scale(${noButtonSize})`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "No 😔",
+    "Are you sure? 🥺",
+    "Pookie please 🥹",
+    "Don't do this to hamodik 😭",
+    "Don't breaking my heart 💔",
+    "Don't say that to batmank... 😭💔",
+  ];
+  return messages[Math.min(noCount, messages.length - 1)];
+}
+
+function changeImage(image) {
+  img.src =
+    image === "yes"
+      ? "https://media.tenor.com/ACi1vdjNbpIAAAAi/%EC%9C%A0%ED%83%80-%ED%86%A4%ED%86%A4%ED%94%84%EB%A0%8C%EC%A6%88.gif"
+      : tontonGifs[image];
+}
+
+function updateNoButtonText() {
+  noBtn.innerHTML = generateMessage(noCount);
+
+}
